2f7c3f9ee2f33529b4adb10916f465cd67eaf808
= <=>
2022-10-25 18:08:47 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navBar">
+        <h1 className="logo">로고</h1>
+        <ul>
+          <li>Home</li>
+          <li>Cart</li>
+        </ul>
+      </nav>
+
+      <div className="main-bg"></div>
+
+      <div className="container-item">
+        <ul>
+          <li>
+            <img
+              src="https://codingapple1.github.io/shop/shoes1.jpg"
+              alt="아이템1"
+              width="60%"
+            />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </li>
+          <li>
+            <img
+              src="https://codingapple1.github.io/shop/shoes2.jpg"
+              alt="아이템3"
+              width="60%"
+            />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </li>
+          <li>
+            <img
+              src="https://codingapple1.github.io/shop/shoes3.jpg"
+              alt="아이템3"
+              width="60%"
+            />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
