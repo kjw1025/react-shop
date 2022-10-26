ed3311907d436493cf0cff068327ca36bd0d30a0
= <=>
2022-10-26 17:51:50 +0900

    라이프사이클 적용

--- src/page/ItemDetail.js
+++ src/page/ItemDetail.js
@@ -1,28 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 export default function ItemDetail({ itemShoes }) {
   let { id } = useParams();
 
-  let returnValue = itemShoes.find(function (data) {
-    console.log(data.id);
-    console.log(itemShoes[id]);
-    // return data.id == itemShoes[id].id;
+  useEffect(() => {
+    setTimeout(() => {
+      let temp = document
+        .querySelector("div.countBox")
+        .classList.add("countBoxClose");
+    }, 2000);
+  });
+
+  let returnValue = itemShoes.find((data) => {
     return data.id == id;
   });
-  console.log(returnValue);
 
   return (
-    <div className="detailItem-wrap baseBox">
-      <div>
-        <img src={returnValue.img} alt="아이템상세이미지" width="100%" />
+    <>
+      <div className="baseBox countBox">2초안에 구매시 할인</div>
+      <div className="detailItem-wrap baseBox">
+        <div>
+          <img src={returnValue.img} alt="아이템상세이미지" width="100%" />
+        </div>
+        <div className="detailItemTxt">
+          <h4>{returnValue.title}</h4>
+          <p>{returnValue.content}</p>
+          <span>{returnValue.price}</span>
+          <button className="orderBt">주문하기</button>
+        </div>
       </div>
-      <div className="detailItemTxt">
-        <h4>{returnValue.title}</h4>
-        <p>{returnValue.content}</p>
-        <span>{returnValue.price}</span>
-        <button className="orderBt">주문하기</button>
-      </div>
-    </div>
+    </>
   );
 }
